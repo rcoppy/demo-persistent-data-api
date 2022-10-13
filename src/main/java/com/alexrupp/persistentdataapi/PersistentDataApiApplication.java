b349ac66a0557fb9da1fd416711753c13885d170
Alex Rupp-Coppi <amr2311@columbia.edu>
2022-10-13 13:48:47 -0400

    spring web boilerplate

---
+++ src/main/java/com/alexrupp/persistentdataapi/PersistentDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.alexrupp.persistentdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistentDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistentDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexrupp/persistentdataapi/PersistentDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexrupp.persistentdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistentDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
